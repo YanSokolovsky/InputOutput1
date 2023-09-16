b0e9695425df10c4bbc3f26a21291764f25fefdf
Yan Sokolovsky <mahuke777@gmail.com>
2023-09-16 12:10:34 +0300

    university labwork-1. the same work, but unit testing edited.

--- src/TeylorRow.java
+++ src/TeylorRow.java
@@ -1,16 +1,43 @@
+import java.math.*;
 
 public class TeylorRow {
-    static double epsilond(int k) {
+    static BigDecimal BigEpsilon(int k) {
+        BigDecimal g = new BigDecimal(1);
+        for (int i = 0; i < k; i++) {
+            g = g.divide(BigDecimal.valueOf(10), k + 1, RoundingMode.HALF_UP);
+        }
+        return g;
+    }
+
+    static double epsilon(int k) {
         double g = 1;
         for (int i = 0; i < k; i++) {
             g = g / 10;
         }
         return g;
     }
+    static BigDecimal BigCalcCos(int k, double x) {
+        int numberForFactorial = 1;
+        BigDecimal X = BigDecimal.valueOf(x).setScale(16, RoundingMode.HALF_UP);
+        BigDecimal epsi = BigEpsilon(k);
+        BigDecimal chlen = BigDecimal.valueOf(1);
+        BigDecimal row = BigDecimal.valueOf(0);
+            while (chlen.abs().compareTo(epsi) > -1) {
+                row = row.add(chlen);
+                chlen = chlen.
+                        multiply(X).
+                        multiply(X).
+                        multiply(BigDecimal.valueOf(-1)).
+                        divide(BigDecimal.valueOf(numberForFactorial), k + 10, RoundingMode.HALF_UP).
+                        divide(BigDecimal.valueOf(numberForFactorial + 1), k + 10, RoundingMode.HALF_UP);
+                numberForFactorial += 2;
+            }
+        return row.setScale(k + 1, RoundingMode.HALF_UP);
+    }
 
     static double calcCos(int k, double x) {
         int numberForFactorial = 1;
-        double epsi = epsilond(k);
+        double epsi = epsilon(k);
         double chlen = 1;
         double row = 0;
         while (Math.abs(chlen) >= epsi) {
@@ -20,7 +47,6 @@ public class TeylorRow {
         }
         return row;
     }
-
     static double compar(int k, double x) {
         double kanon = Math.cos(x);
         double result;
